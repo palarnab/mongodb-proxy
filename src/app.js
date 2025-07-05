8db9fff3f556498544ee12bc29ed8273c79bb65f
Arnab Pal <arnab.i@gmail.com>
2025-07-05 13:20:04 +0530

    fix

--- src/app.js
+++ src/app.js
@@ -1,7 +1,8 @@
+import { MongoClient, ObjectId } from 'mongodb';
+
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv';
 import express from 'express';
-import { MongoClient } from 'mongodb';
-import bodyParser from 'body-parser';
 
 dotenv.config();
 
@@ -9,19 +10,32 @@ export const app = express();
 
 app.use(bodyParser.json());
 
-const allowedTokens = process.env.ALLOWED_TOKENS ? process.env.ALLOWED_TOKENS.split(',') : [];
+const allowedTokens = process.env.ALLOWED_TOKENS
+  ? process.env.ALLOWED_TOKENS.split(',')
+  : [];
+
+app.get('/health', (req, res) => {
+  res.send('🧩 MongoDB Proxy is running');
+});
 
 app.use((req, res, next) => {
   const token = req.headers['authorization'];
-  if (allowedTokens.includes(token.replace('Bearer ', ''))) {
+  if (token && allowedTokens.includes(token.replace('Bearer ', ''))) {
     next();
   } else {
     res.status(401).json({ error: 'Unauthorized' });
   }
 });
 
+app.use((req, res, next) => {
+  const { dbName, collectionName } = req.body;
+  if (!collectionName || !dbName) {
+    res.status(401).json({ error: 'Missing collection or db in body' });
+  } else {
+    next();
+  }
+});
 
-let db;
 let mongoClient;
 
 export const connectDB = async () => {
@@ -29,132 +43,122 @@ export const connectDB = async () => {
     useNewUrlParser: true,
     useUnifiedTopology: true,
   });
-  app.locals.db = mongoClient.db();
   console.log('✅ Mongo connected');
-}
-  
-app.get('/health', (req, res) => {
-  res.send('🧩 MongoDB Proxy is running');
-});
+};
 
 app.post('/create', async (req, res) => {
-  const { collection, index, searchIndex } = req.body;
-  if (!collection) {
-    return res.status(400).json({ error: 'Missing collection or index in body' });
+  const { dbName, collectionName, index, searchIndex } = req.body;
+  const db = mongoClient.db(dbName);
+  await db.createCollection(collectionName);
+  const collection = db.collection(collectionName);
+  if (index) {
+    await collection.createIndex(index);
   }
-
-  try {
-    const collections = await db.listCollections({}, { nameOnly: true }).toArray();
-    if (!collections.some((col) => col.name === collection)) {
-        await db.createCollection(collection);
-        const collection = db.collection(collection);
-        if (index) {
-          await collection.createIndex(index);
-        }
-        if (searchIndex) {
-          await collection.createSearchIndex(searchIndex);
-        }
-        res.status(201).json({ message: `Collection ${collection} created` });
-    }
-  } catch (err) {
-    res.status(500).json({ error: err.message });
+  if (searchIndex) {
+    await collection.createSearchIndex(searchIndex);
   }
+  res.status(201).json({ message: `Collection ${collectionName} created` });
 });
 
 app.delete('/delete', async (req, res) => {
-  const { collection } = req.body;
-  if (!collection) {
-    return res.status(400).json({ error: 'Missing collection in body' });
-  }
+  const { dbName, collectionName } = req.body;
+  const db = mongoClient.db(dbName);
 
-  try {
-    await db.collection(collection).drop();
-    res.status(200).json({ message: `Collection ${collection} deleted` });
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  await db.collection(collectionName).drop();
+  res.status(200).json({ message: `Collection ${collectionName} deleted` });
 });
 
-app.get('/find', async (req, res) => {
-  const { collection, filter, page, pageSize } = req.query;
-  if (!collection) {
-     return res.status(400).json({ error: 'Missing collection param' });
-  }
+app.post('/find', async (req, res) => {
+  const { dbName, collectionName, filter, page, pageSize } = req.body;
+  const db = mongoClient.db(dbName);
   const skip = page ? Number(page) * pageSize : 0;
   const limit = pageSize ? Number(pageSize) || 10 : 10;
-  try {
-    const results = await db.collection(collection).find(filter || {}).skip(skip).limit(limit).toArray();
-    res.json(results);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+
+  const count = await db.collection(collectionName).count(filter || {});
+
+  const results = await db
+    .collection(collectionName)
+    .find(filter || {})
+    .skip(skip)
+    .limit(limit)
+    .toArray();
+
+  res
+    .setHeader('TotalRecords', count)
+    .setHeader('Access-Control-Expose-Headers', `TotalRecords`)
+    .json(results);
 });
 
-app.get('/aggregate', async (req, res) => {
-  const { collection, pipeline } = req.query;
-  if (!collection || !pipeline) {
-     return res.status(400).json({ error: 'Missing collection or pipeline param' });
+app.post('/aggregate', async (req, res) => {
+  const { dbName, collectionName, pipeline } = req.body;
+  if (!pipeline) {
+    return res
+      .status(400)
+      .json({ error: 'Missing collection or pipeline param' });
   }
-  try {
-    const results = await db.collection(collection).aggregate(pipeline).toArray();
-    res.json(results);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const db = mongoClient.db(dbName);
+  const results = await db
+    .collection(collectionName)
+    .aggregate(pipeline)
+    .toArray();
+  res.json(results);
 });
 
 app.post('/insert', async (req, res) => {
-  const { collection, document } = req.body;
-  if (!collection || !document) {
-    return res.status(400).json({ error: 'Missing collection or document in body' });
+  const { dbName, collectionName, document } = req.body;
+  if (!document) {
+    return res
+      .status(400)
+      .json({ error: 'Missing collection or document in body' });
   }
-
-  try {
-    const result = await db.collection(collection).insertOne(document);
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const db = mongoClient.db(dbName);
+  const result = await db.collection(collectionName).insertOne(document);
+  res.status(201).json(result);
 });
 
 app.post('/insertMany', async (req, res) => {
-  const { collection, documents } = req.body;
-  if (!collection || !Array.isArray(documents) || documents.length === 0) {
-    return res.status(400).json({ error: 'Missing collection or documents in body' });
+  const { dbName, collectionName, documents } = req.body;
+  if (!Array.isArray(documents) || documents.length === 0) {
+    return res
+      .status(400)
+      .json({ error: 'Missing collection or documents in body' });
   }
-
-  try {
-    const result = await db.collection(collection).insertMany(documents);
-    res.status(201).json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const db = mongoClient.db(dbName);
+  const result = await db.collection(collectionName).insertMany(documents);
+  res.status(201).json(result);
 });
 
-app.put('/findByIdAndUpdate', async (req, res) => {
-  const { collection, id, update } = req.body;
-  if (!collection || !id || !update) {
-    return res.status(400).json({ error: 'Missing collection, id or update in body' });
+app.post('/findByIdAndUpdate', async (req, res) => {
+  const { dbName, collectionName, id, update } = req.body;
+  if (!id || !update) {
+    return res
+      .status(400)
+      .json({ error: 'Missing collection, id or update in body' });
   }
-
-  try {
-    const result = await db.collection(collection).findOneAndUpdate({ _id: id }, { $set: update }, { returnOriginal: true });
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+  const db = mongoClient.db(dbName);
+  const result = await db
+    .collection(collectionName)
+    .findOneAndUpdate(
+      { _id: new ObjectId(id) },
+      { $set: update },
+      { returnNewDocument: true },
+    );
+  res.status(200).json(result);
 });
 
 app.delete('/findByIdAndDelete', async (req, res) => {
-  const { collection, id } = req.body;
-  if (!collection || !id) {
+  const { dbName, collectionName, id } = req.body;
+  if (!id) {
     return res.status(400).json({ error: 'Missing collection or id in body' });
   }
+  const db = mongoClient.db(dbName);
+  const result = await db
+    .collection(collectionName)
+    .findOneAndDelete({ _id: new ObjectId(id) });
+  res.status(200).json(result);
+});
 
-  try {
-    const result = await db.collection(collection).findOneAndDelete({ _id: id });
-    res.status(200).json(result);
-  } catch (err) {
-    res.status(500).json({ error: err.message });
-  }
+app.use((err, req, res, next) => {
+  console.error(err.message, err);
+  res.status(500).json({ error: err.message });
 });
